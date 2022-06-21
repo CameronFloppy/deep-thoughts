f2d3ddec093f3223f7c25e26229c266aca555ab6
Jay Ford <jay.ford45@gmail.com>
2022-06-21 18:22:21 -0400

    working commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,50 @@
+const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+    Query: {
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {}
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id })
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        } 
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args)
+
+            return user;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email })
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            return user;
+        }
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+}
+type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+}
+type User {
+    _id: ID
+    username : String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+}
+type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+}
+type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
+}
+`;
+
+module.exports = typeDefs;
