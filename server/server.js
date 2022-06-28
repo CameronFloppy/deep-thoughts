852dc0899072ba0585c65d795f81204902797f8c
Jay Ford <jay.ford45@gmail.com>
2022-06-28 15:13:48 -0400

    finished front end features

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -18,6 +19,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+}); 
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start()
